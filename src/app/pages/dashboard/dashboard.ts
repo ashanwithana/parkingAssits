ccf359750ff1f940501fef20b22b84407f68e2dc
AshanSaberion <ashan@saberion.lk>
2025-07-04 23:18:24 +0530

    feat: Add response and site models, enhance dashboard with filtering options

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { Master } from '../../services/master/master';
+import { IResponseModel } from '../../models/res.model';
+import { ISite } from '../../models/site.model';
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+
+  siteList: ISite[] = [];
+  masterService = inject(Master)
+
+  ngOnInit(): void {
+    this.getSites()
+  }
+
+  getSites() {
+    return this.masterService.getSitesByClientId().subscribe((res: IResponseModel) => {
+      this.siteList = res.data;
+    })
+  }
 
 }
